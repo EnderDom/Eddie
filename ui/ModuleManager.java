8c9101ac2f100030860a8fd76757682da5ced5ca
Dominic Wood <dominicwood1988@googlemail.com>
2012-02-07 08:42:55 +0000

    Latest Module

--- ui/ModuleManager.java
+++ ui/ModuleManager.java
@@ -1,8 +1,12 @@
 package ui;
 
+import gui.EddieGUI;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -12,6 +16,8 @@ import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import cli.EddieCLI;
 
 import tools.Tools_XML;
 
@@ -24,8 +30,12 @@ public class ModuleManager {
 	Document xmlModuleList;
 	private String modulefolder;
 	private boolean loaded;
-	NodeList modules;
 	Module[] instantiated;
+	HashMap<String, String> module_classpath;
+	HashMap<String, String> module_actions;
+	HashMap<String, String> module_tasks;
+	HashMap<String, String> module_task_classes;
+	
 	
 	public ModuleManager(String modulefolder){
 		this.modulefolder = modulefolder;
@@ -53,15 +63,106 @@ public class ModuleManager {
 			}
 		}
 		else{
-			Logger.getRootLogger().error("No Module Xml file!!");
+			Logger.getRootLogger().warn("No Module Xml file!!");
+			Logger.getRootLogger().warn("Building default XML with standard modules");
 			return false;
 		}
 	}
 	
 	//Go through xml and pulls out all the classes
 	public void loadModules(){
-		if(loaded)modules = xmlModuleList.getElementsByTagName("MOD");
+		//TODO
+		/*
+		 * Load all default modules here
+		 */
+		
+		//Load additional modules here
+		if(loaded){
+			NodeList modules = xmlModuleList.getElementsByTagName("MOD");
+			
+			for(int i =0;i < modules.getLength(); i++){
+				NodeList module_info = modules.item(i).getChildNodes();
+				String modname = new String("");
+				for(int j =0;j < module_info.getLength(); j++){
+					if(module_info.item(i).getNodeName().contentEquals("MOD_NAME")){
+						modname = module_info.item(i).getTextContent();
+					}
+					else if(module_info.item(i).getNodeName().contentEquals("MOD_CLASSPATH")){
+						String classpath = module_info.item(i).getTextContent();
+						if(modname.length() > 0 && classpath.length() > 0){
+							module_classpath.put(modname, classpath);
+						}
+						else{
+							Logger.getRootLogger().error("Module XML file is corrupt");
+						}
+					}
+				}
+			}
+		}
 		else Logger.getRootLogger().error("Module XML not loaded. Why is this still being called???");
+	}
+	
+	public void setupGUI(EddieGUI gui){
+		for(String key : module_classpath.keySet()){
+			try {
+				Module temp =(Module)Class.forName(module_classpath.get(key)).getConstructor().newInstance();
+				temp.addToGui(gui);
+				//TODO add actions to list
+				//TODO add tasks to list
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void setupCLI(EddieCLI cli){
+		for(String key : module_classpath.keySet()){
+			try {
+				Module temp =(Module)Class.forName(module_classpath.get(key)).getConstructor().newInstance();
+				temp.addToCli(cli);
+				//TODO add actions to list
+				//TODO add tasks to list
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void checkInstalls(){
@@ -69,6 +170,14 @@ public class ModuleManager {
 	}
 	
 	public void addActions(){
+		
+	}
+	
+	public void addModule(){
+		
+	}
+	
+	public void defaultInstall(){
 		
 	}
 	
