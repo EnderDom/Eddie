11a52c2a18609505e19ada808f0d01059abd2261
Dominic Wood <dominicwood1988@googlemail.com>
2012-03-14 08:34:00 +0000

    Added Math_Tools Methods

--- tools/Tools_Math.java
+++ tools/Tools_Math.java
@@ -8,4 +8,16 @@ public class Tools_Math {
 		return d/Math.pow(10,c);
 	}
 	
+	public static double average(int[] i){
+		double l = 0;
+		l=(double)sum(i);
+		return l/(double)i.length;
+	}
+	
+	public static int sum(int[] i){
+		int l = 0;
+		for(int j =0;j < i.length; j++)l=l+i[j];
+		return l;
+	}
+	
 }
