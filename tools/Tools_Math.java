7efdc304f54f1d87d775d3a57b82d51bc7518250
Dominic Wood <dominicwood1988@googlemail.com>
2012-03-27 19:01:52 +0100

    Math Tools Additions

--- tools/Tools_Math.java
+++ tools/Tools_Math.java
@@ -20,4 +20,29 @@ public class Tools_Math {
 		return l;
 	}
 	
+	public static boolean isOdd(int i){
+		return (i % 2 == 0) ? true : false;
+	}
+	
+	/*
+	 * Calculates the overlap between two 1D lines
+	 * x1, x2 is start and finish of 1 line
+	 * x3, x4 is start and finish of other line
+	 * Result will be percentage
+	 * Negative percentage indicates no overlap
+	 * 
+	 */
+	public static double getOverlap(int x1, int x2, int x3, int x4){
+		int x5 = Math.abs(Math.min(x1,x2)-Math.min(x3,x4));
+		int x6 = Math.abs(Math.max(x1,x2)-Math.max(x3,x4));
+		int xMax = Math.max(Math.max(x1,x2),Math.max(x3,x4));
+		int xMin = Math.min(Math.min(x1,x2), Math.min(x3,x4));
+		int xSize = xMax-xMin;
+		int overlap = xSize-x5-x6;
+		double y1 = (double)overlap;
+		double y2 = (double)xSize;
+		double y3 = y1/y2;
+		return y3;
+	}
+	
 }
